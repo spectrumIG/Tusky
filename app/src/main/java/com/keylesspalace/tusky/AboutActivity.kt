9145bef97be23451258ef74a46591e32a7211d07
charlag <charlag@tuta.io>
2020-04-12 18:07:51 +0200

    Add log sending functionality

--- app/src/main/java/com/keylesspalace/tusky/AboutActivity.kt
+++ app/src/main/java/com/keylesspalace/tusky/AboutActivity.kt
@@ -1,8 +1,22 @@
+/* Copyright 2017 Andrew Dawson
+ *
+ * This file is a part of Tusky.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Tusky; if not,
+ * see <http://www.gnu.org/licenses>. */
+
 package com.keylesspalace.tusky
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.annotation.StringRes
 import android.text.SpannableString
 import android.text.SpannableStringBuilder
 import android.text.method.LinkMovementMethod
@@ -10,11 +24,18 @@ import android.text.style.URLSpan
 import android.text.util.Linkify
 import android.view.MenuItem
 import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.core.content.FileProvider
+import androidx.core.net.toFile
 import com.keylesspalace.tusky.di.Injectable
 import com.keylesspalace.tusky.util.CustomURLSpan
+import com.keylesspalace.tusky.util.ProcessExecutionException
+import com.keylesspalace.tusky.util.getLogFile
 import com.keylesspalace.tusky.util.hide
 import kotlinx.android.synthetic.main.activity_about.*
 import kotlinx.android.synthetic.main.toolbar_basic.*
+import java.io.IOException
 
 class AboutActivity : BottomSheetActivity(), Injectable {
 
@@ -32,7 +53,7 @@ class AboutActivity : BottomSheetActivity(), Injectable {
 
         versionTextView.text = getString(R.string.about_app_version, getString(R.string.app_name), BuildConfig.VERSION_NAME)
 
-        if(BuildConfig.CUSTOM_INSTANCE.isBlank()) {
+        if (BuildConfig.CUSTOM_INSTANCE.isBlank()) {
             aboutPoweredByTusky.hide()
         }
 
@@ -48,6 +69,9 @@ class AboutActivity : BottomSheetActivity(), Injectable {
             startActivityWithSlideInAnimation(Intent(this, LicenseActivity::class.java))
         }
 
+        sendLogsButton.setOnClickListener {
+            sendLogs()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -60,6 +84,29 @@ class AboutActivity : BottomSheetActivity(), Injectable {
         return super.onOptionsItemSelected(item)
     }
 
+    private fun sendLogs() {
+        val logFileUri = try {
+            getLogFile(this)
+        } catch (e: Exception) {
+            if (e is IOException || e is ProcessExecutionException) {
+                Toast.makeText(this, "Could not read logs", Toast.LENGTH_SHORT)
+                return
+            } else {
+                throw e
+            }
+        }
+        val shareFileUri = FileProvider.getUriForFile(applicationContext,
+                "${BuildConfig.APPLICATION_ID}.fileprovider", logFileUri.toFile())
+
+        val sendIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_STREAM, shareFileUri)
+            type = "text/plain"
+        }
+
+        startActivity(Intent.createChooser(sendIntent,
+                getText(R.string.action_send_logs)))
+    }
 }
 
 private fun TextView.setClickableTextWithoutUnderlines(@StringRes textId: Int) {
